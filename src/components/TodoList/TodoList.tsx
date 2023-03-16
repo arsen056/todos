f675dfe076e676982eaa7f6956c526ecfc68e395
arsen056 <vurdihanov@gmail.com>
2023-03-16 17:25:29 +0300

    feat: add get filtered tasks function

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -6,13 +6,17 @@ import {addTask} from 'reducer/reducer';
 import {AddItem} from 'components/AddItem/AddItem';
 import uuid from 'react-uuid';
 
+import {getFilteredTasks} from 'utils/getFilteredTasks';
+
 import s from './TodoList.module.css';
 
 export const TodoList = () => {
 
   const {todos, dispatch} = useContext(Context);
+  
+  const filteredTasks = getFilteredTasks(todos.filter, todos.tasks);
 
-  const tasksMap = todos.tasks.map(t => <Task key={t.id} id={t.id} title={t.title} isDone={t.isDone} />);
+  const tasksMap = filteredTasks.map(t => <Task key={t.id} id={t.id} title={t.title} isDone={t.isDone} />);
 
   const addNewTask = (title: string) => {
     const newTask: TaskType = {id: uuid(), title, isDone: false};
@@ -26,6 +30,9 @@ export const TodoList = () => {
       <ul>
         {tasksMap}
       </ul>
+      <button>all</button>
+      <button>active</button>
+      <button>completed</button>
     </div>
   );
 };
