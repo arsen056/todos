edaf79541ad005bb07bf7dc95b30680354d04bcb
arsen056 <vurdihanov@gmail.com>
2023-03-17 10:28:04 +0300

    feat: add getFilteredTasks test

---
+++ src/__tests__/getFilteredTasks.ts
@@ -0,0 +1,25 @@
+import {TaskType} from 'types';
+import uuid from 'react-uuid';
+import {getFilteredTasks} from 'utils/getFilteredTasks';
+
+const tasks: TaskType[] = [
+  {id: uuid(), title: 'React', isDone: false},
+  {id: uuid(), title: 'Html', isDone: false},
+  {id: uuid(), title: 'Css', isDone: false},
+  {id: uuid(), title: 'JS', isDone: true}
+];
+
+test('Should be all tasks', () => {
+  const filteredTasks = getFilteredTasks('all', tasks);
+  expect(filteredTasks.length).toBe(4);
+});
+
+test('Should be active tasks', () => {
+  const filteredTasks = getFilteredTasks('active', tasks);
+  expect(filteredTasks.length).toBe(3);
+});
+
+test('Should be completed tasks', () => {
+  const filteredTasks = getFilteredTasks('completed', tasks);
+  expect(filteredTasks.length).toBe(1);
+});
